bbdeca6cd05b1a5a552acd6d568ebbca6e10eb60
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2024-09-11 22:33:05 +0200

    trying for the footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,17 +1,39 @@
-export default function Footer({ blok }) {
+// export default function Footer({ blok }) {
 
-    const text = blok.title.content[0].content[0].text
+import Link from "next/link";
+
+//     const text = blok.title.content[0].content[0].text
+//     return (
+//         <section className="w-full bg-red">
+//             <p>{text}</p>
+
+//             {blok.searchbox && blok.searchbox.map((item, index) => {
+
+
+//         return <p >{item.minitext}</p>
+
+//             })}
+//         </section>
+//     )
+// }
+
+// github
+export default function Footer({ links }) {
+    if (!Array.isArray(links)) {
+      console.error('Expected links to be an array, but got:', links);
+      return null; // or handle the error gracefully
+    }
+  
     return (
-        <section className="w-full bg-red">
-            <p>{text}</p>
-
-            {blok.searchbox && blok.searchbox.map((item, index) => {
-
-
-        return <p >{item.minitext}</p>
-
-            })}
-        </section>
-    )
-}
-
+      <footer>
+        <ul>
+          {blok.searchbox && blok.searchbox.map((link, index) => (
+            <li key={link.id || index}>
+              <Link href={link.url}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </footer>
+    );
+  }
+  
