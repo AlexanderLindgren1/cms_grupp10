b1268ccba3faffdbb5882b0be18d7f9784fc5c94
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2024-09-13 14:57:00 +0200

    test

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,36 +1,7 @@
-// import Link from "next/link";
 
-//  export default function Header({ blok }) {
-
-//     // console.log("in header",blok.searchbox[0].searchbox);
-
-//     return (
-//         <section className="w-full bg-red">
-//             <p>header</p>
-
-//             {blok.links.map((linkstory)=>{
-
-//                 const story =  linkstory?.link?.story
-
-//             return (
-
-//                     <Link key={story?._uid} href={story?.slug}>
-//                     <p> {story?.name}</p>
-//                     </Link>
-
-//             )
-//             })
-
-//             }
-
-//             <input type="text" placeholder={blok?.searchbox[0]?.searchbox} />
-//         </section>
-//     )
-// }
-
-// chatgpts
 import Link from "next/link";
 import "../styles/style.css";
+import Image from "next/image";
 export default function Header({ blok }) {
   console.log(blok);
 
@@ -50,7 +21,12 @@ export default function Header({ blok }) {
           );
         })}
         <form className="search-form">
-          <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAPRJREFUSEu1lAEOwiAMRdnJ1JOpJ9ObqS/hLw2WAhuQmOFG/vttabe0eG2L9VMEuKaU+F2yCfbPvH/0GqsBELg3RIA1QR7Air+Na/YSFLwJKQFW/PZLEaLesudCSAn4ZLWms1yfVz5fNWMBctUjrqgAqPhuPc4CEAdCKonib1mA0jPSG0MAhRsV1zMZGrNujwBUt64UNQ879psXw0agfKLTkybbC9W6RY0WXVdrZqjRcF92qd4hymJMaM//av75eHbYMWmBdRW5NnPkWm55khbNKd0+FzLSVNH0rkJmAYC7c2kmAIjqsUc7GxAOuyjHh78tj+ALspM/GVK5SjgAAAAASUVORK5CYII=" />
+        <Image src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAPRJREFUSEu1lAEOwiAMRdnJ1JOpJ9ObqS/hLw2WAhuQmOFG/vttabe0eG2L9VMEuKaU+F2yCfbPvH/0GqsBELg3RIA1QR7Air+Na/YSFLwJKQFW/PZLEaLesudCSAn4ZLWms1yfVz5fNWMBctUjrqgAqPhuPc4CEAdCKonib1mA0jPSG0MAhRsV1zMZGrNujwBUt64UNQ879psXw0agfKLTkybbC9W6RY0WXVdrZqjRcF92qd4hymJMaM//av75eHbYMWmBdRW5NnPkWm55khbNKd0+FzLSVNH0rkJmAYC7c2kmAIjqsUc7GxAOuyjHh78tj+ALspM/GVK5SjgAAAAASUVORK5CYII=" 
+       
+       
+          width={768}
+          height={768} 
+          layout="responsive" />
           <input type="text" placeholder={blok.searchbox[0].searchbox} />
         </form>
       </nav>
