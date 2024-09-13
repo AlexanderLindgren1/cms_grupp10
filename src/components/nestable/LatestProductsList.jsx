99047df89223e926a0bc4712352115d53de6b979
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2024-09-13 15:02:56 +0200

    added GEN

--- src/components/nestable/LatestProductsList.jsx
+++ src/components/nestable/LatestProductsList.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
+
+const [product, setProduct] = useState([])
+
 export default function LatestProductsList({ blok }) {
-  const storySlug = "products/product1"
   
   
-  fetch(
+    const storySlug = "products/product1"
+  
+  
+  const GEN =fetch(
     `https://api.storyblok.com/v2/cdn/stories/${storySlug}?version=published&token=${process.env.NEXT_PUBLIC_PREVIEW_STORYBLOK_TOKEN}`
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log("Storyblok-svar:", data);
+        setProduct(data)
     })
     .catch((error) => {
       console.error("Fel vid hämtning från Storyblok:", error);
