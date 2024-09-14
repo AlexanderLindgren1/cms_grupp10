4d4aa4b5f1f2295304ccbbc2346cb4cfaf789f95
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2024-09-14 10:16:34 +0200

    added done latest

--- src/components/nestable/LatestProductsList.jsx
+++ src/components/nestable/LatestProductsList.jsx
@@ -1,21 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+import "../styles/latestProStyle.css"
 
-function xx(data){
-console.log("in xx", data);
-
-}
 export default function LatestProductsList({ blok }) {
-  console.log("hardcoder", blok.latestpro[0]);
 
   return (
-    <div>
+    <div className="latestHolder">
       {blok?.latestpro?.map((lp, index) => (
         <Link
           key={index}
           href={`${lp.text}`}>
-          <img
-            src={lp?.image?.filename || "/default-image.jpg"}
+          <Image
+          className="latestitem"
+            src={lp?.image?.filename }
             width={400}
             height={400}
             alt="latest Image"
