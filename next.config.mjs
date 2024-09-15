1ae05c50d960e50febdf148338df6f920bb4622c
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2024-09-15 12:45:23 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["a.storyblok.com"],
-        formats: ["image/avif", "image/webp"],
+  images: {
+    domains: ["a.storyblok.com"],
+    formats: ["image/avif", "image/webp"],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml',
+        destination: '/api/sitemap',
       },
-      
+    ];
+  },
 };
 
 export default nextConfig;
