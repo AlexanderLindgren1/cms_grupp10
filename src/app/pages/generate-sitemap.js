6cf17446aab3b42386cf5c13f6d8ecf36f765827
AlexanderLindgren1 <alexprestoxxx@gmail.com>
2024-09-15 12:48:30 +0200

    remoevd cooments

--- src/app/pages/generate-sitemap.js
+++ src/app/pages/generate-sitemap.js
@@ -1,19 +1,18 @@
 import { SitemapStream, streamToPromise } from 'sitemap';
 import { Readable } from 'stream';
-import StoryblokCMS from './path-to-your-storyblok-class'; // Adjust the path to your StoryblokCMS class
+import StoryblokCMS from './path-to-your-storyblok-class'; 
 
 export async function generateSitemap() {
   const cms = new StoryblokCMS();
-  const products = await cms.getProducts(); // Fetch your products or any other pages you want to include
+  const products = await cms.getProducts(); 
   
   const links = products.map(product => ({
-    url: `/products/${product.full_slug}`,  // Adjust based on your routing
+    url: `/products/${product.full_slug}`,  
     changefreq: 'weekly',
     priority: 0.8,
   }));
 
   const stream = new SitemapStream({ hostname: 'https://cms-grupp10.vercel.app/' });
 
-  // Create a stream with URLs to include in the sitemap
   return streamToPromise(Readable.from(links).pipe(stream)).then(data => data.toString());
 }
